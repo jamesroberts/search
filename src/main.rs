a794bf6158a58efab22944b829e812731c901d85
James Roberts <jamesroberts.dev@gmail.com>
2023-04-07 21:51:56 +0200

    Fix TF-IDF calculation

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,15 @@ use lexer::Token;
 use logos::Logos;
 
 
-use std::{fs::{read_dir, ReadDir, DirEntry, read_to_string}, path::{Path, PathBuf}, collections::HashMap};
+use std::{fs::{read_dir, ReadDir, DirEntry, read_to_string}, path::{Path, PathBuf}, collections::HashMap, env};
 struct Files;
+
+#[derive(Clone, Debug)]
+struct Document {
+    doc_path: PathBuf,
+    token_freq_map: HashMap<String, usize>,
+    token_count: usize
+}
 
 impl FileProvider for Files {
     fn files(dir: &Path) -> ReadDir {
@@ -26,46 +33,52 @@ fn main() {
     
     let mut index: Index = HashMap::new();
     let mut idf: IDF = HashMap::new();
+    let mut documents = Vec::new();
     
     println!("Indexing...");
-    let mut documents = Vec::new();
     for entry in files {
         let file = entry.unwrap();
         if let Some(doc) = process_file(&file) {
-            index.insert(file.path(), doc.token_freq_map);
+            index.insert(doc.doc_path.clone(), doc.token_freq_map.clone());
             documents.push(doc);
         }
     }
 
     for tf_map in index.values() {
-        for &token in tf_map.keys() {
-            let f: f32 = idf.get(&token).unwrap_or(&0.0) + 1.0;
-            idf.insert(token, f);
-            idf.insert(token, calculate_idf(token, &documents));
+        for token in tf_map.keys() {
+            if !idf.contains_key(token) {
+                idf.insert(token.clone(), calculate_idf(token.clone(), &documents));
+            }
         }
     }
 
-    let mut tfidf: HashMap<String, Vec<(&PathBuf, f32)>> = HashMap::new();
-
-    for (token, idf) in idf.iter() {
-        for (path, tf_map) in index.iter() {
-            if tf_map.contains_key(token) {
-                let score = idf * tf_map.get(token).unwrap();
-
-                let mut ranking = tfidf.clone().get(token).unwrap_or(&Vec::new()).to_owned();
-                ranking.push((path, score));
-                ranking.sort_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap());
-                tfidf.insert(token.clone(), ranking);
-            } 
-        }
-    }
-
-    //dbg!(&index);
-    //dbg!(idf);
-    //dbg!(tfidf);
-    println!("Search for 'James': ");
-    // dbg!(tfidf.get("James"));
+    let mut tfidf: HashMap<String, Vec<(&Document, f32)>> = HashMap::new();
     
+    for (token, idf) in idf.iter() {
+        let mut doc_ranking: Vec<(&Document, f32)> = Vec::new();
+        for doc in documents.iter() {
+            let tf = calculate_tf(token.clone(), doc);
+            let score = if idf != &0.0 { idf * tf } else { tf }; 
+            doc_ranking.push((doc, score));
+        }
+        doc_ranking.sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap());
+        tfidf.insert(token.clone(), doc_ranking);
+    }
+    
+    println!("Finished indexing!");
+    let args: Vec<String> = env::args().collect();
+    let search_token = &args[1].to_string();
+
+    match tfidf.get(search_token) {
+        Some(results) => {
+            println!("Search results for '{search_token}':");
+            for (doc, score) in results {
+                let path = doc.doc_path.to_str().unwrap();
+                println!("    Score: {score} => Path: {path}",);
+            }
+        },
+        None => println!("Token not found in documents")
+    }
 }
 
 fn process_file(file: &DirEntry) -> Option<Document> {
@@ -78,33 +91,29 @@ fn process_file(file: &DirEntry) -> Option<Document> {
 
     // Maybe depending on the file type we can dispatch to a content parser
     let content = read_to_string(file.path()).expect("Should be able to read");
-    println!("==================");
     
     let mut tf: TF = HashMap::new();
-    let mut doc = Document {
-        token_freq_map: tf,
-        token_count: 0
-    };
     let mut lexer = Token::lexer(&content);
-    while let Some(token_type) = lexer.next() {
-        doc.token_count += 1;
+    let mut count = 0;
+    while let Some(_token_type) = lexer.next() {
+        count += 1;
         let token = lexer.slice().to_string();
-        tf.insert(token, tf.get(&token).unwrap_or(&0) + 1);
-        println!("TOKEN => {token_type:?} = {token:?}");
+        tf.insert(token.clone(), tf.get(&token).cloned().unwrap_or(0) + 1);
+        // println!("TOKEN => {_token_type:?} = {token:?}");
     }
-    return Some(doc);
+    Some(Document {
+        doc_path: file.path(),
+        token_freq_map: tf,
+        token_count: count
+    })
 }
 
-struct Document {
-    token_freq_map: HashMap<String, usize>,
-    token_count: usize
-}
 
 // https://en.wikipedia.org/wiki/Tf%E2%80%93idf
 fn calculate_tf(token: String, doc: &Document) -> f32 {
+    if doc.token_count == 0 { return 0.0; }
     let token_freq = doc.token_freq_map.get(&token).cloned().unwrap_or(0);
-    let total_tokens = if doc.token_count > 0 {doc.token_count} else {1};
-    (token_freq / total_tokens) as f32
+    return token_freq as f32 / doc.token_count as f32
 }
 
 fn calculate_idf(token: String, docs: &Vec<Document>) -> f32 {
@@ -114,4 +123,3 @@ fn calculate_idf(token: String, docs: &Vec<Document>) -> f32 {
     });
     (n / m).log10()
 }
-
